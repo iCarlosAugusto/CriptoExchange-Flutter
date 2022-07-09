9dae1f6c4f467439ebc68ce0597e203ef150be60
carlos <carlosaugustodelimavieira@protonmail.com>
2022-07-09 00:05:34 -0300

    feat: :sparkles: Add select and remove functionalits

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,22 +2,39 @@ import 'package:cripto_exchange/models/coin.dart';
 import 'package:flutter/material.dart';
 import '../repository/coinRepository.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final table = CoinRepository.table;
+  List<Coin> selectedCoin = [];
 
   Widget build(BuildContext context) {
-    final table = CoinRepository.table;
-
     return Scaffold(
       appBar: AppBar(title: const Text("Home Page")),
       body: ListView.separated(
           itemBuilder: (BuildContext content, int index) {
-            return  ListTile(
+            return ListTile(
               title: Text(table[index].name),
-              leading: Image.asset(table[index].image, width: 40),
+              leading: selectedCoin.contains(table[index]) 
+                ? const CircleAvatar(child: Icon(Icons.check)) 
+                : SizedBox(child: Image.asset(table[index].image, width: 40,)),
               trailing: Text(table[index].value),
+              selected: selectedCoin.contains(table[index]),
+              onTap: () {
+                setState(() {
+                  if (selectedCoin.contains(table[index])) {
+                    selectedCoin.remove(table[index]);
+                  } else {
+                    selectedCoin.add(table[index]);
+                  }
+                });
+              },
             );
           },
           separatorBuilder: (_, __) => const Divider(),
